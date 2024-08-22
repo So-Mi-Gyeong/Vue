9ed6298c01100ac995f15d5ea21a631430b828d2
mkso <mk.so@kbhc.co.kr>
2024-08-22 17:32:22 +0900

    [수정] vue

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import store from './store'
 
 import "@/assets/scss/common.scss";
+import "@/assets/js/ui.global.js";
 
 createApp(App).use(store).use(router).mount('#app')
