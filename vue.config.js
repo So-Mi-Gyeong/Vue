553fb5157393acc759f1e13770a03a1eb27af76d
mkso <mk.so@kbhc.co.kr>
2024-11-26 11:32:20 +0900

    test

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/[repository-name]/'
-    : '/Vue/'  // 개발 모드에서는 /Vue/ 경로를 사용
+  transpileDependencies: true
 })
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Vue/'
+    : '/'
+}
